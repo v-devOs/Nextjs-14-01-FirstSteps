b07437ad0971c11f8a9212fc0bc0939bfd5faa9a
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2024-06-18 21:33:38 -0600

    Metadata en next13

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,10 @@
+import type { Metadata } from "next";
 
-
+export const metadata: Metadata = {
+  title: 'SEO Title',
+  description: 'SEO Description',
+  keywords: ['About Page', 'Uriel', 'informacion']
+};
 
 export default function AboutPage() {
   return (
