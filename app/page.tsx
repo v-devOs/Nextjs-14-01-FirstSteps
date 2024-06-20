46aa9a3711b84566aef44c0bccc45a0bbcfa1bdb
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2024-06-20 12:43:10 -0600

    Docker basic

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <span className="text-5xl">Hola mundo</span>
+
+      <Link href={'/about'}>About Page</Link>
     </main>
   );
 }
